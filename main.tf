96a7afb231ce2f0aeeb478c4364297f1d5281acd
Senthil <senthil.sc@gmail.com>
2021-07-24 12:10:51 +0530

    change 1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.0.2" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,11 +9,11 @@ terraform {
    }
 
 provider "aws" {
-       # region = "us-east-1" 
+        region = "us-east-1" 
         profile = "default"
 	}
 
-resource "aws_instance" "Raminfra" {
+resource "aws_instance" "tfCICDinstance" {
 	ami = "ami-0dc2d3e4c0f9ebd18" 
 	instance_type = "t2.micro"
 }
