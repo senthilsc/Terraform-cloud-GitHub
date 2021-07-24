650492bb0d536fddd040a4217d78e89f50cfd30e
Senthil <senthil.sc@gmail.com>
2021-07-24 13:28:06 +0530

    destroy plan

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
    }
 
 provider "aws" {
-        region = "us-west-1" 
+        region = "us-east-1" 
         profile = "default"
 	}
 
 resource "aws_instance" "tfCICDinstance" {
-	# ami = "ami-0dc2d3e4c0f9ebd18" # ami for east region
-  ami = "ami-0ed05376b59b90e46"
+	ami = "ami-0dc2d3e4c0f9ebd18" # ami for east region
+  # ami = "ami-0ed05376b59b90e46" # ami for west region
   # count = 3
 	instance_type = "t2.micro"
 }
