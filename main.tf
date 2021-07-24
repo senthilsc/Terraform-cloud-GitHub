5b13e73cc59b577afddb800804e323c055977714
Senthil <senthil.sc@gmail.com>
2021-07-24 13:35:49 +0530

    commit

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_instance" "tfCICDinstance" {
 	ami = "ami-0dc2d3e4c0f9ebd18" # ami for east region
-  # ami = "ami-0ed05376b59b90e46" # ami for west region
+  # ami = "ami-0ed05376b59b90e46" # ami for west region #
   # count = 3
 	instance_type = "t2.micro"
 }
